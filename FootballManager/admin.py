8aee0acb9fd1a8be5e538198b7bcc9ff04ac3ec2
Arkadiusz Mroczkowski <80561@student.pb.edu.pl>
2024-04-11 14:45:37 +0200

    Database fields shown on Admin panel

---
+++ FootballManager/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Footballer)
+admin.site.register(Event)
+admin.site.register(Match)
+admin.site.register(Queue)
+admin.site.register(Team)
+admin.site.register(Statistic)
+
