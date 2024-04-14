8ed832793169faa66548249da98eca27092396ca
Mateusz Jurkowicz <93523950+MateuszJurkowicz@users.noreply.github.com>
2024-04-14 16:42:13 +0200

    Update settings.py

--- FootballManager/settings.py
+++ FootballManager/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'FootballManager',
         'USER': 'root',
-        'PASSWORD': 'mati123',
+        'PASSWORD': '',
         'HOST':'localhost',
         'PORT':'3306',
     }
