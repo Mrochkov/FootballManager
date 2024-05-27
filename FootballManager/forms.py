36b906123ed8df7dfece4be3839facbdbef504a0
Arkadiusz Mroczkowski <80561@student.pb.edu.pl>
2024-05-27 10:04:36 +0200

    Adding and editing logo and description for team, admin panel filter and search for team

--- FootballManager/forms.py
+++ FootballManager/forms.py
@@ -9,15 +9,17 @@ class FootballerForm(ModelForm):
         fields = '__all__'
 
 
-class TeamForm(ModelForm):
+class TeamForm(forms.ModelForm):
     class Meta:
         model = Team
-        fields = ['name', 'trainer']
+        fields = ['name', 'trainer', 'logo', 'description']
+
 
 class EventForm(ModelForm):
     class Meta:
         model = Queue
         fields = '__all__'
+
 
 class MatchForm(ModelForm):
     class Meta:
@@ -32,15 +34,18 @@ class MatchForm(ModelForm):
             raise forms.ValidationError("Drużyna gospodarzy nie może być taka sama jak drużyna gości.")
         return cleaned_data
 
+
 class MatchResultForm(ModelForm):
     class Meta:
         model = Match
         fields = ['host_goals', 'guest_goals']
 
+
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ['minute', 'event_type', 'footballer']
+
 
 class QueueForm(ModelForm):
     class Meta:
