aa4bdc353fbb92fb49b95aeb157ac94a17d9b5ed
Arkadiusz Mroczkowski <80561@student.pb.edu.pl>
2024-05-29 16:17:56 +0200

    Updated settings

--- FootballManager/settings.py
+++ FootballManager/settings.py
@@ -25,9 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('DJANGO_SECRET_KEY', default='default-secret-key-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'footballmanager-amlj.onrender.com',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
