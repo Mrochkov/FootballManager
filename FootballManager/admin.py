29cb867eccca5ca25c673df555efdae15f5d50f0
Arkadiusz Mroczkowski <80561@student.pb.edu.pl>
2024-05-29 14:40:28 +0200

    Admin panel filters and search

--- FootballManager/admin.py
+++ FootballManager/admin.py
@@ -16,7 +16,7 @@ class QueueInline(admin.TabularInline):
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('name', 'wins', 'draws', 'losses', 'goals_scored', 'goals_lost', 'trainer', 'matches_played')
     search_fields = ('name', 'trainer')
-    list_filter = ('trainer',)
+    list_filter = ('trainer', 'wins', 'name')
     fields = ('name', 'trainer', 'wins', 'draws', 'losses', 'goals_scored', 'goals_lost', 'logo', 'description')
     inlines = [FootballerInline]
 
@@ -24,6 +24,8 @@ class TeamAdmin(admin.ModelAdmin):
 @admin.register(Footballer)
 class FootballerAdmin(admin.ModelAdmin):
     list_display = ('name', 'surname', 'team')
+    search_fields = ('name', 'surname', 'team__name')
+    list_filter = ('team',)
 
 
 class EventInline(admin.TabularInline):
@@ -34,17 +36,21 @@ class EventInline(admin.TabularInline):
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
     list_display = ('host_team', 'guest_team', 'date', 'host_goals', 'guest_goals')
+    search_fields = ('host_team__name', 'guest_team__name')
+    list_filter = ('host_team', 'guest_team', 'date')
     inlines = [EventInline]
-
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ('minute', 'footballer', 'match', 'event_type')
+    search_fields = ('footballer__name', 'footballer__surname')
+    list_filter = ('event_type',)
 
 
 @admin.register(Queue)
 class QueueAdmin(admin.ModelAdmin):
     list_display = ('id', 'amount_of_matches')
+    search_fields = ('matches__id',)
 
     def amount_of_matches(self, obj):
         return obj.matches.count()
@@ -53,3 +59,4 @@ class QueueAdmin(admin.ModelAdmin):
 @admin.register(Statistic)
 class Statistic(admin.ModelAdmin):
     list_display = ('footballer', 'matches_played', 'goals_scored', 'assists', 'yellow_cards', 'red_cards')
+    search_fields = ('footballer__name', 'footballer__surname')
